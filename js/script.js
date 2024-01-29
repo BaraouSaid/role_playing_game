52b5a107e2270850d4cc2e5e32029b91cc62ca5a
Said <slaoualy@yahoo.fr>
2024-01-29 17:38:42 +0100

    feat: tested the fn goStore with a console.log

--- js/script.js
+++ js/script.js
@@ -16,4 +16,6 @@ const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
 
-function goStore() {}
+function goStore() {
+  console.log('Going to store.');
+}
