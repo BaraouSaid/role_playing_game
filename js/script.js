a38b750025b552bc42f10eef7bd1f0787683f553
Said <slaoualy@yahoo.fr>
2024-02-01 07:27:56 +0100

    feat: Implemented the if statement of the buyWeapon function

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
+
 const weapons = [
   {
     name: 'stick',
@@ -98,6 +99,13 @@ function buyHealth() {
 
 function buyWeapon() {
   if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeapon].name;
+    text.innerText = `You now have a ${newWeapon}.`;
+    inventory.push(newWeapon);
+    text.innerText += ' In your inventory you have: ' + inventory[0];
   }
 }
 
