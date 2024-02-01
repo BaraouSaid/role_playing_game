720c05899c8f7c7ab5e379196b958912bcaa26cc
Said <slaoualy@yahoo.fr>
2024-02-01 22:10:07 +0100

    fix: fixed buttons initialization

--- js/script.js
+++ js/script.js
@@ -119,5 +119,5 @@ function fightBeast() {}
 
 // initialize buttons
 button1.onclick = goStore();
-button1.onclick = goStore();
-button1.onclick = goStore();
+button2.onclick = goCave();
+button3.onclick = fightDragon();
