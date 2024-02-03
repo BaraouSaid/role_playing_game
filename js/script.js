4941ad7f2afdf4147bc9c71194da22bae1f92929
Said <slaoualy@yahoo.fr>
2024-02-03 21:53:13 +0100

    feat: Implemented the sellWeapon function

--- js/script.js
+++ js/script.js
@@ -121,6 +121,12 @@ function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
+    //To get the first element of the inventory array
+    let currentWeapon = inventory.shift();
+    text.innerText = 'You sold a ' + currentWeapon + '.';
+    text.innerText += ' In your inventory you have: ' + inventory + '.';
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
