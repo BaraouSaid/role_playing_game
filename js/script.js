9f8e86731bb34523cf32330015db81e500b483c2
Said <slaoualy@yahoo.fr>
2024-02-04 05:24:29 +0100

    feat: Implemented the attack function

--- js/script.js
+++ js/script.js
@@ -178,6 +178,20 @@ function fightDragon() {
   goFight();
 }
 
-function attack() {}
+function attack() {
+  text.innerText = `The ${monsters[fighting].name} attacks.`;
+  text.innerText += ` You attack it with your ${weapons[currentWeapon].name}.`;
+  health -= monsters[fighting].level;
+  //Substract from monsterHealth the power of current weapon + xp value
+  monsterHealth -=
+    weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
+}
 
 function dodge() {}
